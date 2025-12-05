9f90e298be21d4c86a13166b07a361169e9509ac
Tiemen Schuijbroek <t.j.l.schuijbroek@ratio-case.nl>
2025-12-05 21:42:39 +0100

    fix: add missing HopSlotMap implementation to reactive_stores
    Signed-off-by: Tiemen Schuijbroek <t.j.l.schuijbroek@ratio-case.nl>

--- reactive_stores/src/slotmap.rs
+++ reactive_stores/src/slotmap.rs
@@ -21,6 +21,16 @@ impl<K: slotmap::Key, V> KeyedAccess for slotmap::DenseSlotMap<K, V> {
         self.get_mut(key).expect("key does not exist")
     }
 }
+impl<K: slotmap::Key, V> KeyedAccess for slotmap::HopSlotMap<K, V> {
+    type Key = K;
+    type Value = V;
+    fn keyed(&self, key: Self::Key) -> &Self::Value {
+        self.get(key).expect("key does not exist.")
+    }
+    fn keyed_mut(&mut self, key: Self::Key) -> &mut Self::Value {
+        self.get_mut(key).expect("key does not exist")
+    }
+}
 impl<K: slotmap::Key, V> KeyedAccess for slotmap::SecondaryMap<K, V> {
     type Key = K;
     type Value = V;
